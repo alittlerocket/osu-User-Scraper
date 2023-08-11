f146bf62f7d727346ef0f5ea09fab7d2ee5d3bca
alittlerocket <zhangm21@rpi.edu>
2023-08-11 13:48:52 -0400

    delete useless scraperV2 file.

--- scraperV2.py
+++
@@ -1,9 +0,0 @@
-from ossapi import Ossapi, UserLookupKey, GameMode, RankingType
-
-client_id = 22250
-client_secret = 'EFa2u6qrI8nD6LpHOmtYKnaUVjTEFvrk2GVvZs4u'
-api = Ossapi(client_id, client_secret)
-
-user_list = api.user(10652591)
-print(user_list)
-print(user_list[0].country.name)
